dea1d9c028dc713c48dffc5b1c0b76d667c9b60d
khatiainanashvili <khatiainanashvili@itvet.ge>
2023-04-30 22:31:40 +0400

    add todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/todo/" element={<Home />} />
+        <Route path="/todo/login" element={<Login />} />
       </Routes>
     </>
   );
