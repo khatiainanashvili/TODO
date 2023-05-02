af20316aa630bd43a95303f8bff6f1f5a22d614f
khatiainanashvili <khatiainanashvili@itvet.ge>
2023-05-03 00:33:03 +0400

    add: go home button styles

--- src/components/Page404.jsx
+++ src/components/Page404.jsx
@@ -7,7 +7,7 @@ export default function Page404() {
       <h1> 404 </h1>
       <h2>Page Not Found</h2>
       <Link to="/">
-        <button className="btn">Go to Home</button>
+        <button className="btn go-home-btn">Go to Home</button>
       </Link>
     </div>
   );
